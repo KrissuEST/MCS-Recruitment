db00c0b49f5b4376a4f6e1cf6a3836fccf2516ee
KrissuEST <kristjantonismae@gmail.com>
2016-11-22 21:42:57 +0200

    First files and changes.

---
+++ src/app/weather-station/weather-station.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './weather-station.html',
+    styleUrls: ['./weather-station.css']
+})
+export class WeatherStationComponent {
+	public status = "Work in progress";
+	public time_updated = "Siia tuleb info!";
+}
