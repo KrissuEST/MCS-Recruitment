03995c5816e49c3bdc0d62a14def8e7b62c22544
KrissuEST <kristjantonismae@gmail.com>
2016-11-28 14:32:47 +0200

    Second extra changes.

--- src/app/weather-station/weather-station.component.ts
+++ src/app/weather-station/weather-station.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     templateUrl: './weather-station.html',
+	dataUrl: './wstation.json',
     styleUrls: ['./weather-station.css']
 })
 export class WeatherStationComponent {
 	public status = "Work in progress";
-	public time_updated = "Siia tuleb info!";
+	public time_updated = "2016-08-08 15:57:50";
 }
